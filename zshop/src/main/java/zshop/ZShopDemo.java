fbfa75fb011478b19cbde3f26d656741fd989c5f
Priya Shimpi <priya.ravindra.shimpi@gmail.com>
2019-12-31 12:16:13 +0530

    first Commit

---
+++ zshop/src/main/java/zshop/ZShopDemo.java
@@ -0,0 +1,22 @@
+package zshop;
+
+public class ZShopDemo {
+private int Income;
+private int Profit;
+public ZShopDemo() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+public int getIncome() {
+	return Income;
+}
+public void setIncome(int income) {
+	Income = income;
+}
+public int getProfit() {
+	return Profit;
+}
+public void setProfit(int profit) {
+	Profit = profit;
+}
+}
