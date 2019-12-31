289f41c8a0c0490778b6a1f89ea03ad83ade8b1f
Priya Shimpi <priya.ravindra.shimpi@gmail.com>
2019-12-31 12:44:33 +0530

    Second Commit

--- zshop/src/main/java/zshop/ZShopDemo.java
+++ zshop/src/main/java/zshop/ZShopDemo.java
@@ -1,6 +1,10 @@
 package zshop;
 
 public class ZShopDemo {
+@Override
+	public String toString() {
+		return "ZShopDemo [Income=" + Income + ", Profit=" + Profit + "]";
+	}
 private int Income;
 private int Profit;
 public ZShopDemo() {
